09475d5adce6101b07c846331f5cac9bc1c492a2
Christopher Munden <cmunden@unca.edu>
2016-04-27 21:37:01 -0400

    Started the work on getting a functional Searchbar

--- map.js
+++ map.js
@@ -2,6 +2,7 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 0, lng: 0},
     zoom: 3,
+       mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: [{
       featureType: 'poi',
       stylers: [{ visibility: 'off' }]  // Turn off points of interest.
@@ -12,3 +13,11 @@ function initMap() {
     disableDoubleClickZoom: true
   });
 }
+
+  var input = document.getElementById('tfheader');
+var searchBox = new google.maps.places.SearchBox(input);
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
