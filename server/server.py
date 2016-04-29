ed883fa146294e8de7a37bad4787240eae523500
Jason Andrae <jasonsandrae@gmail.com>
2016-04-29 19:26:14 -0400

    Working on getting server up. Connected server to Mongo

--- server/server.py
+++ server/server.py
@@ -1,39 +1,45 @@
 # code modified from Flask documentation: Armin Ronacher http://flask.pocoo.org
 # all the imports
-import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash, jsonify
 from contextlib import closing
+from mongokit import Connection, Document
 import time
 
 # configuration
-DATABASE = 'feedingLog.db'
-DEBUG = True
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
 
 # create application
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 # setup connection for database
-def connect_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-		db = g._database = sqlite3.connect(DATABASE)
-		db.row_factory = sqlite3.Row
-    return db
+connection = Connection(app.config['MONGODB_HOST'],
+                        app.config['MONGODB_PORT'])
 
-# function called before each request
-# g is object imported from flask
-@app.before_request
-def before_request():
-    g.db = connect_db()
+def max_length(length):
+    def validate(value):
+        if len(value) <= length:
+            return True
+        raise Exception('%s must be at most %s characters long' % length)
+    return validate
 
-# function to be called after a response has been constructed to handle exceptions
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        print "Teardown {0!r}".format(exception)
-        db.close()
+class User(Document):
+    structure = {
+        'username': unicode,
+        'password': unicode,
+    }
+    validators = {
+        'name': max_length(20),
+        'email': max_length(15)
+    }
+    use_dot_notation = True
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
+# register the User document with our current connection
+connection.register([User])
 
 # function to return entire log from database
 @app.route('/log')
@@ -81,12 +87,3 @@ def initiate_feeding(name):
 # run server
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug='true')
-
-# One time setup for creating database
-# initalize database by opening local folder and creating db from saved schema file
-def init_db():
-    with app.app_context():
-        db = connect_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
