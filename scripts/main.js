c188033e1f7d0c3305d69ae28f8fb220305686bd
Jason Andrae <jasonsandrae@gmail.com>
2016-04-28 21:44:47 -0400

    Starting to create shell for future functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,25 @@
+//Temporary global varriables for debug purposes
+var latitude;
+var longitude;
+var username;
+var password;
+
+var debug=true;
+
+// Function to initialize the page and set event handlers
+function init(){
+
+}
+
 // Function to run on page Load
 $(document).ready(function(){
   loginPopup();
+  init();
 });
-
 
 // Function to automatically resize all elements within a resized window
 function rescaleWindow() {
     console.log("Resizing window ");
-
 }
 
 // Function to delay window resizing to a fixed interval to reduce usage
