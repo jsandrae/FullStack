abbcb05d33149a6da2bf89e353f208c0eab3aa10
Hunter Hall <m.hunter.hall.17@gmail.com>
2016-04-30 11:44:54 -0400

    updated list.js

--- scripts/list.js
+++ scripts/list.js
@@ -1,12 +1,20 @@
 $('button#signInButton') .on('click',function() {
-  $('#login-box').fadeOut(300);
   
+  //Saving the username and password
   var username;
   var password;
   
   username = $('#username').text;
   password = $('#password').text;
   
- dismissLogin(username,password);
-
+  var isValid = dismissLogin(username,password);
+  
+  if (isValid) { 
+    $('#login-box').fadeOut(300);}
+  else {
+    var $incorrect = $('<p>');
+    $incorrect.text = 'Sorry, incorrect login.';
+    $incorrect.insertBefore($('#signInButton'));
+  }
+  
 });
