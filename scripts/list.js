90ebad31b1c43f7cbf18fd9a02386baf52cd1ca5
Hunter Hall <m.hunter.hall.17@gmail.com>
2016-04-30 11:27:43 -0400

    no message

--- scripts/list.js
+++ scripts/list.js
@@ -1,3 +1,11 @@
 $('button#signInButton') .on('click',function() {
   $('#login-box').fadeOut(300);
+  
+  var username;
+  var password;
+  
+  username = $('#username').text;
+  password = $('#password').text;
+  
+  console.log($('#username').val());
 });
